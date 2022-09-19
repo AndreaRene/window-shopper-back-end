00511ff0ebd3be4a9832638a122e3faf033fc50e
Andrea <andrea@interhact.net>
2022-09-19 17:00:21 -0500

    models complete. work on routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+    res.json(categoryData)
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
   // find all categories
   // be sure to include its associated Products
 });
